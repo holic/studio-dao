d57dead71eefff275e2a63361b6c30c2a437ac5d
Kevin Ingersoll <kingersoll@gmail.com>
2022-10-23 09:53:03 -0700

    bring connectkit back in

--- packages/app/src/ConnectWalletButton.tsx
+++ packages/app/src/ConnectWalletButton.tsx
@@ -1,3 +1,4 @@
+import { ConnectKitButton } from "connectkit";
 import { useAccount, useConnect, useDisconnect } from "wagmi";
 
 import { Button } from "./Button";
@@ -8,32 +9,32 @@ type Props = {
 };
 
 export const ConnectWalletButton = ({ label = "Connect" }: Props) => {
-  const { address } = useAccount();
-  const { connect, connectors } = useConnect();
-  const { disconnect } = useDisconnect();
   const isMounted = useIsMounted();
 
   // Avoid flash of unconnected wallet
   if (!isMounted) return null;
 
   return (
-    <Button
-      size="sm"
-      variant={address ? "secondary" : "primary"}
-      className="self-start sm:self-auto"
-      onClick={() => {
-        address ? disconnect() : connect({ connector: connectors[0] });
-      }}
-    >
-      <span className="flex-grow min-w-0 ">
-        {address ? (
-          <span className="max-w-[10rem] overflow-hidden text-ellipsis block">
-            {address}
+    <ConnectKitButton.Custom>
+      {({ isConnected, isConnecting, show, address, ensName }) => (
+        <Button
+          size="sm"
+          variant={address ? "secondary" : "primary"}
+          className="self-start sm:self-auto"
+          onClick={show}
+          pending={isConnecting}
+        >
+          <span className="flex-grow min-w-0 ">
+            {isConnected ? (
+              <span className="max-w-[10rem] overflow-hidden text-ellipsis block">
+                {ensName || address}
+              </span>
+            ) : (
+              label
+            )}
           </span>
-        ) : (
-          label
-        )}
-      </span>
-    </Button>
+        </Button>
+      )}
+    </ConnectKitButton.Custom>
   );
 };
--- packages/app/src/EthereumProviders.tsx
+++ packages/app/src/EthereumProviders.tsx
@@ -1,33 +1,24 @@
-import {
-  configureChains,
-  createClient,
-  defaultChains,
-  WagmiConfig,
-} from "wagmi";
-import { alchemyProvider } from "wagmi/providers/alchemy";
-import { publicProvider } from "wagmi/providers/public";
+import { ConnectKitProvider, getDefaultClient } from "connectkit";
+import { allChains, chain, createClient, WagmiConfig } from "wagmi";
 
-// Will default to goerli if nothing set in the ENV
 export const targetChainId =
-  parseInt(process.env.NEXT_PUBLIC_CHAIN_ID || "0") || 5;
+  parseInt(process.env.NEXT_PUBLIC_CHAIN_ID || "") || chain.goerli.id;
 
-// filter down to just mainnet + optional target testnet chain so that rainbowkit can tell
-// the user to switch network if they're on an alternative one
-const targetChains = defaultChains.filter(
-  (chain) => chain.id === 1 || chain.id === targetChainId
+const targetChains = allChains.filter((chain) => chain.id === targetChainId);
+// add mainnet after target chain so we don't force a network switch after connecting
+// and so that ENS can resolve via mainnet
+targetChains.push(chain.mainnet);
+
+export const wagmiClient = createClient(
+  getDefaultClient({
+    appName: "StudioDAO",
+    alchemyId: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY,
+    chains: targetChains,
+  })
 );
 
-const { provider, webSocketProvider } = configureChains(targetChains, [
-  alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY }),
-  publicProvider(),
-]);
-
-export const wagmiClient = createClient({
-  autoConnect: true,
-  provider,
-  webSocketProvider,
-});
-
 export const EthereumProviders: React.FC = ({ children }) => (
-  <WagmiConfig client={wagmiClient}>{children}</WagmiConfig>
+  <WagmiConfig client={wagmiClient}>
+    <ConnectKitProvider theme="midnight">{children}</ConnectKitProvider>
+  </WagmiConfig>
 );
