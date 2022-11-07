fe55b81cf013f54fda44030865f304c143f244ff
James Burns <james@>
2022-11-06 19:00:27 -0800

    Update JB Graph provider

--- packages/app/src/juicebox/JuiceboxGraphProvider.tsx
+++ packages/app/src/juicebox/JuiceboxGraphProvider.tsx
@@ -5,7 +5,7 @@ import {
 } from "urql";
 
 const graphClient = createGraphClient({
-  url: "https://api.studio.thegraph.com/query/31944/jb-v3-goerli/0.0.80",
+  url: "https://api.studio.thegraph.com/query/30654/goerli-dev/0.2.0",
 });
 
 type Props = {
