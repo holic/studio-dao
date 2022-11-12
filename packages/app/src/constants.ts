d58c25438cbbc7477973b858a1cb938106236c7f
James Burns <james@>
2022-11-11 20:38:16 -0800

    Graph url for production

--- packages/app/src/constants.ts
+++ packages/app/src/constants.ts
@@ -3,7 +3,7 @@ import { chain } from "wagmi";
 import { targetChainId } from "./EthereumProviders";
 
 export const juiceboxTreasuryIds =
-  targetChainId === chain.goerli.id ? [124] : [331];
+  targetChainId === chain.goerli.id ? [124] : [311];
 
 export const juiceboxProjectIds =
   targetChainId === chain.goerli.id
--- packages/app/src/juicebox/JuiceboxGraphProvider.tsx
+++ packages/app/src/juicebox/JuiceboxGraphProvider.tsx
@@ -4,8 +4,12 @@ import {
   Provider as GraphProvider,
 } from "urql";
 
+const graphClientURL =
+  process.env.NEXT_PUBLIC_GRAPH_CLIENT_URL ||
+  "https://api.studio.thegraph.com/query/30654/goerli-dev/0.3.0";
+
 const graphClient = createGraphClient({
-  url: "https://api.studio.thegraph.com/query/30654/goerli-dev/0.3.0",
+  url: graphClientURL,
 });
 
 type Props = {
