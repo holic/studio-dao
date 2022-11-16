4ea813e60010acf29c183820c1c52b382f1d7340
James Burns <james@>
2022-11-16 15:43:30 -0800

    Add GA

--- packages/app/src/Head.tsx
+++ packages/app/src/Head.tsx
@@ -1,11 +1,31 @@
 // import { SEO } from '../SEO'
+import Script from "next/script";
 
 export const Head: React.FC = () => {
+  const gaMeasurementId = process.env.NEXT_PUBLIC_CHAIN_ID;
   const siteDescription =
     "StudioDAO is  a movie studio owned by filmmakers and fans. Members purchase Super Tickets and vote on which movies receive weekly community grants. Grow your Greenlight Power by contributing to the community.";
 
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gaMeasurementId}`}
+      />
+      <Script
+        id="ga"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gaMeasurementId}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <meta
         key="viewport"
         name="viewport"
