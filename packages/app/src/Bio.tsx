673d02b9073d78f1d77bd906719405e370a16dd6
James Burns <james@>
2022-12-01 14:37:29 -0800

    funding goal

--- packages/app/src/Bio.tsx
+++ packages/app/src/Bio.tsx
@@ -9,9 +9,9 @@ export const BioCard = ({ name, bio, imageUrl }: Props) => (
     <div className="flex flex-col md:flex-row overflow-hidden">
       <img
         src={imageUrl}
-        className="flex-shrink-0   h-80  aspect-square object-cover rounded-lg"
+        className="flex-shrink-0 max-w-sm   h-80  aspect-square object-cover rounded-lg"
       />
-      <div className="flex-grow flex flex-col justify-between px-8 md:px-12 xs:p-8 gap-8">
+      <div className="flex-grow flex flex-col justify-between lg:px-8 md:px-8 py-4 gap-8">
         <div className="space-y-4">
           <h5 className="font-dmserif text-3xl text-white">{name}</h5>
           <p></p>
