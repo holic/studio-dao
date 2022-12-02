d4f0c1e793c77e9a9f3be993967d39512e1da1aa
James Burns <james@>
2022-12-02 14:24:24 -0800

    meta is optional

--- packages/app/src/Meta.tsx
+++ packages/app/src/Meta.tsx
@@ -1,11 +1,5 @@
 import Head from "next/head";
 
-/**
- * Meta components need to be referenced as functions
- * Because Next.js's <Head> component does not work with
- * react components.
- */
-
 export type MetaArgs = {
   type?: string;
   title?: string;
@@ -34,72 +28,80 @@ const defaults: MetaArgs = {
 };
 
 type Props = {
-  meta?: MetaArgs;
+  metadata?: MetaArgs;
 };
 
-export const Meta = ({ meta }: Props) => (
-  <Head>
-    <meta
-      key="viewport"
-      name="viewport"
-      content="width=device-width, initial-scale=1"
-    />
-    <title>{meta.title ? meta.title : defaults.title}</title>
-    <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <meta name="referrer" content="always" />
-    <meta
-      name="description"
-      content={meta.description ? meta.description : defaults.description}
-    />
-    <meta
-      name="keywords"
-      content={meta.description ? meta.description : defaults.description}
-    />
-    <link rel="icon" href="/favicon.ico" />
+export const Meta = ({ metadata }: Props) => {
+  const meta = metadata ? metadata : defaults;
+  return (
+    <Head>
+      <meta
+        key="viewport"
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+      />
+      <title>{meta.title ? meta.title : defaults.title}</title>
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+      <meta name="referrer" content="always" />
+      <meta
+        name="description"
+        content={meta.description ? meta.description : defaults.description}
+      />
+      <meta
+        name="keywords"
+        content={meta.description ? meta.description : defaults.description}
+      />
+      <link rel="icon" href="/favicon.ico" />
 
-    <meta name="twitter:card" content={"summary_large_image"} />
-    <meta
-      name="twitter:site"
-      content={meta.twitterHandle ? meta.twitterHandle : defaults.twitterHandle}
-    />
-    <meta
-      name="twitter:site:id"
-      content={meta.twitterId ? meta.twitterId : defaults.twitterId}
-    />
-    <meta
-      name="twitter:image"
-      content={meta.image ? meta.image.url : defaults.image?.url}
-    />
-    <meta
-      name="twitter:title"
-      content={meta.title ? meta.title : defaults.title}
-    />
+      <meta name="twitter:card" content={"summary_large_image"} />
+      <meta
+        name="twitter:site"
+        content={
+          meta.twitterHandle ? meta.twitterHandle : defaults.twitterHandle
+        }
+      />
+      <meta
+        name="twitter:site:id"
+        content={meta.twitterId ? meta.twitterId : defaults.twitterId}
+      />
+      <meta
+        name="twitter:image"
+        content={meta.image ? meta.image.url : defaults.image?.url}
+      />
+      <meta
+        name="twitter:title"
+        content={meta.title ? meta.title : defaults.title}
+      />
 
-    <meta property="og:type" content={meta.type ? meta.type : defaults.type} />
-    <meta
-      property="og:site_name"
-      content={meta.title ? meta.title : defaults.title}
-    />
-    <meta
-      property="og:image"
-      content={meta.image ? meta.image.url : defaults.image?.url}
-    />
-    <meta
-      property="og:image:width"
-      content={meta.image ? meta.image.width : defaults.image?.width}
-    />
-    <meta
-      property="og:image:height"
-      content={meta.image ? meta.image.height : defaults.image?.height}
-    />
-    <meta
-      property="og:title"
-      content={meta.title ? meta.title : defaults.title}
-    />
-    <meta
-      property="og:description"
-      content={meta.description ? meta.description : defaults.description}
-    />
-    <meta property="og:url" content={meta.url ? meta.url : defaults.url} />
-  </Head>
-);
+      <meta
+        property="og:type"
+        content={meta.type ? meta.type : defaults.type}
+      />
+      <meta
+        property="og:site_name"
+        content={meta.title ? meta.title : defaults.title}
+      />
+      <meta
+        property="og:image"
+        content={meta.image ? meta.image.url : defaults.image?.url}
+      />
+      <meta
+        property="og:image:width"
+        content={meta.image ? meta.image.width : defaults.image?.width}
+      />
+      <meta
+        property="og:image:height"
+        content={meta.image ? meta.image.height : defaults.image?.height}
+      />
+      <meta
+        property="og:title"
+        content={meta.title ? meta.title : defaults.title}
+      />
+      <meta
+        property="og:description"
+        content={meta.description ? meta.description : defaults.description}
+      />
+      <meta property="og:url" content={meta.url ? meta.url : defaults.url} />
+    </Head>
+  );
+};
