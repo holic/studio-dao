2bc245289cf0d479a4294ed788ff028b2be72ae7
James Burns <james@>
2022-12-08 17:26:28 -0800

    Use total paid

--- packages/app/src/juicebox/TreasuryBalance.tsx
+++ packages/app/src/juicebox/TreasuryBalance.tsx
@@ -1,18 +1,8 @@
 import { ethers } from "ethers";
-import { gql } from "urql";
 
 import { useTreasuryBalanceQuery } from "../../codegen/juicebox";
 import { juiceboxTreasuryIds } from "../constants";
 import { PendingIcon } from "../icons/PendingIcon";
-
-gql`
-  query TreasuryBalance($projectIds: [Int!]!) {
-    projects(where: { projectId_in: $projectIds }) {
-      id
-      currentBalance
-    }
-  }
-`;
 
 export const TreasuryBalance = () => {
   const [queryResult] = useTreasuryBalanceQuery({
@@ -22,7 +12,7 @@ export const TreasuryBalance = () => {
   });
 
   const balance = queryResult.data?.projects
-    ?.map((p) => ethers.BigNumber.from(p.currentBalance))
+    ?.map((p) => ethers.BigNumber.from(p.totalPaid))
     .reduce((a, b) => a.add(b), ethers.BigNumber.from(0));
 
   return (
